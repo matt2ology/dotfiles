6a4c2583a1a9a23b127fd79288fb925663a1fa96
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-08-07 14:07:52 -0700

    Added some custom command functions

--- dot_shell/aliases.sh
+++ dot_shell/aliases.sh
@@ -16,3 +16,35 @@ alias gag='git exec ag'
 
 # chezmoi aliases - https://www.chezmoi.io/user-guide/command-overview/
 alias cz='chezmoi'
+
+# ============================================================================
+# Custom commands - A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+# ============================================================================
+
+# Check if a file contains non-ascii characters
+nonascii() {
+    LC_ALL=C grep -n '[^[:print:][:space:]]' "${1}"
+}
+
+# Go up [n] directories
+up()
+{
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number"
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive"
+    else
+        for ((i=0; i<${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
+}
