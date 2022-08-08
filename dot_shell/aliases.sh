8de52fded56af71c2f8eb912a6e9d6bdc27b7258
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-08-07 22:22:21 -0700

    Coreutils utilities colors in .shell/aliases.sh

--- dot_shell/aliases.sh
+++ dot_shell/aliases.sh
@@ -2,10 +2,6 @@
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-# Use colors in coreutils utilities output
-alias ls='ls --color=auto'
-alias grep='grep --color'
-
 # ls aliases
 alias ll='ls -aFhl' # all, classiFy, human readable file size, long list format
 alias la='ls -A' # list Almost all
@@ -21,6 +17,20 @@ alias gag='git exec ag'
 # chezmoi aliases - https://www.chezmoi.io/user-guide/command-overview/
 alias cz='chezmoi'
 alias czcd='chezmoi cd' # Launch a shell in the working tree
+
+# Enable color support of ls and also add handy aliases
+# This is a list of commands from the GNU Core Utilities for Unix environments.
+# These commands can be found on Unix operating systems and most Unix-like operating systems
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
 
 # ============================================================================
 # Custom commands - A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
