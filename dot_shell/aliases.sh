62eece0a70fb7b1263d70eb3a441147f0c068cf8
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-08-07 21:03:47 -0700

    Add an "alert" alias for long running commands

--- dot_shell/aliases.sh
+++ dot_shell/aliases.sh
@@ -1,3 +1,7 @@
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
 # Use colors in coreutils utilities output
 alias ls='ls --color=auto'
 alias grep='grep --color'
