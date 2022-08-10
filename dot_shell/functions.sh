e07e75bc2af8dc322bc0bd84277f2c4e017d9d78
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-08-10 00:16:00 -0700

    Funcs added to append or prepend paths to $PATH

--- dot_shell/functions.sh
+++ dot_shell/functions.sh
@@ -1,3 +1,27 @@
+##############################################################################
+# Useful functions
+# Shell Agnostic. Should work both with Bash and ZSH
+# https://ryanstutorials.net/bash-scripting-tutorial/bash-functions.php
+##############################################################################
+
+# https://linuxhint.com/path_in_bash/
+# https://stackoverflow.com/questions/370047/what-is-the-most-elegant-way-to-remove-a-path-from-the-path-variable-in-bash
+function path_remove() {
+    PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
+}
+
+function path_append() {
+    # Does the alias only if the aliased program is installed
+    path_remove "$1"
+    PATH="${PATH:+"$PATH:"}$1"
+}
+
+function path_prepend() {
+    # Does the alias only if the aliased program is installed
+    path_remove "$1"
+    PATH="$1${PATH:+":$PATH"}"
+}
+
 # Save current location, save here, save this location, hold my spot
 function here() {
     local loc
